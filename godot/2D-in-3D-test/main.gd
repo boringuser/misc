29d2db3f8b7c31627021771c8c1ea95e2b9a4b84
boringuser <eric@Erics-MacBook-Pro.local>
2017-09-22 09:05:08 -0700

    Adding Godot 2D in 3D test

---
+++ godot/2D-in-3D-test/main.gd
@@ -0,0 +1,6 @@
+extends Spatial
+
+func _ready():
+	var tex = get_node("Viewport").get_render_target_texture()
+	get_node("Quad").get_material_override().set_texture(FixedMaterial.PARAM_DIFFUSE,tex)
+	pass
